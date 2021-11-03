f8cf747166e75a14dbb1b7b966403febac09d32a
Sveinung Gundersen <sveinugu@gmail.com>
2021-11-03 17:51:43 +0100

    Added support for specifying theme colors with opacity

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,6 +12,54 @@
 <script>
 import siteHeader from '~/components/siteHeader.vue'
 import siteFooter from '~/components/siteFooter.vue'
+
+export function forEach(arg, func) {
+  Array.prototype.forEach.call(arg, func)
+}
+export function isObject(arg) {
+  return Object.prototype.toString.call(arg) === '[object Object]'
+}
+
+// Modified from: https://github.com/vuetifyjs/vuetify/issues/9282#issuecomment-735378004
+
+export const hexToRgb = (h) => {
+  let r = 0
+  let g = 0
+  let b = 0
+
+  if (h.length === 4) {
+    r = '0x' + h[1] + h[1]
+    g = '0x' + h[2] + h[2]
+    b = '0x' + h[3] + h[3]
+  } else if (h.length === 7) {
+    r = '0x' + h[1] + h[2]
+    g = '0x' + h[3] + h[4]
+    b = '0x' + h[5] + h[6]
+  }
+  return `${+r}, ${+g}, ${+b}`
+}
+
+export const generateRgbVar = (name, color) => {
+  const colorRgb = hexToRgb(color)
+  if (!colorRgb.includes('NaN')) {
+    document.documentElement.style.setProperty(`--v-${name}-rgb`, colorRgb)
+  }
+}
+
+export const createRgbVarsForThemes = (themes) => {
+  for (const theme of Object.values(themes)) {
+    for (const [key, colors] of Object.entries(theme)) {
+      if (isObject(colors)) {
+        for (const [colorKey, color] of Object.entries(colors)) {
+          generateRgbVar(`${key}-${colorKey}`, color)
+        }
+      } else {
+        generateRgbVar(key, colors)
+      }
+    }
+  }
+}
+
 export default {
   components: {
     siteHeader,
@@ -26,6 +74,9 @@ export default {
   //     ],
   //   }
   // },
+  mounted() {
+    createRgbVarsForThemes(this.$vuetify.theme.themes)
+  },
 }
 </script>
 
