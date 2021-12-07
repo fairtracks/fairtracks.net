fceb8998d3a4ad82384d89715b720db01dacd7e8
Sveinung Gundersen <sveinugu@gmail.com>
2021-12-07 03:29:29 +0100

    Added QuoteText component

---
+++ components/QuoteText.vue
@@ -0,0 +1,41 @@
+<template>
+  <p>
+    <v-row>
+      <v-col cols="1">
+        <v-row>
+          <v-icon x-large class="grey--text text--lighten-1"
+            >mdi-format-quote-open
+          </v-icon>
+        </v-row>
+      </v-col>
+      <v-col cols="10">
+        <v-row
+          :class="
+            $vuetify.theme.dark ? 'black' : 'white grey--text text--darken-2'
+          "
+          class="ma-0 text-h6 font-italic text-center"
+        >
+          {{ quote }}
+        </v-row>
+      </v-col>
+      <v-col cols="1">
+        <v-row>
+          <v-icon x-large class="grey--text text--lighten-1"
+            >mdi-format-quote-close
+          </v-icon>
+        </v-row>
+      </v-col>
+    </v-row>
+  </p>
+</template>
+
+<script>
+export default {
+  props: {
+    quote: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
