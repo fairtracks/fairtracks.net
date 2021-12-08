3c93969688ad5c7154dac8a0d18f4cffee6ffb64
Sveinung Gundersen <sveinugu@gmail.com>
2021-12-08 11:04:43 +0100

    Moved logo

--- components/Logo.vue
+++ components/Logo.vue
@@ -5,6 +5,8 @@
     height="100%"
     :aspect-ratio="1532 / 480"
     tag="img"
-    :src="require('~/assets/logos/FAIRtracks-logo-light-white.png')"
+    :src="
+      require('~/assets/illustrations/logos/FAIRtracks-logo-light-white.png')
+    "
   />
 </template>
