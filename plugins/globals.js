7dccc1476ac7b7d6cc451fa79261aac17ca00707
Sveinung Gundersen <sveinugu@gmail.com>
2021-12-08 17:27:03 +0100

    Moved style generation functions to global.js mixin

--- plugins/globals.js
+++ plugins/globals.js
@@ -1,7 +1,50 @@
 import Vue from 'vue'
 
 Vue.mixin({
+  mounted() {
+    this.createRgbVarsForThemes(this.$vuetify.theme.themes)
+  },
   methods: {
+    isObject(arg) {
+      return Object.prototype.toString.call(arg) === '[object Object]'
+    },
+
+    // Modified from: https://github.com/vuetifyjs/vuetify/issues/9282#issuecomment-735378004
+    hexToRgb(h) {
+      let r = 0
+      let g = 0
+      let b = 0
+
+      if (h.length === 4) {
+        r = '0x' + h[1] + h[1]
+        g = '0x' + h[2] + h[2]
+        b = '0x' + h[3] + h[3]
+      } else if (h.length === 7) {
+        r = '0x' + h[1] + h[2]
+        g = '0x' + h[3] + h[4]
+        b = '0x' + h[5] + h[6]
+      }
+      return `${+r}, ${+g}, ${+b}`
+    },
+    generateRgbVar(name, color) {
+      const colorRgb = this.hexToRgb(color)
+      if (!colorRgb.includes('NaN')) {
+        document.documentElement.style.setProperty(`--v-${name}-rgb`, colorRgb)
+      }
+    },
+    createRgbVarsForThemes(themes) {
+      for (const theme of Object.values(themes)) {
+        for (const [key, colors] of Object.entries(theme)) {
+          if (this.isObject(colors)) {
+            for (const [colorKey, color] of Object.entries(colors)) {
+              this.generateRgbVar(`${key}-${colorKey}`, color)
+            }
+          } else {
+            this.generateRgbVar(key, colors)
+          }
+        }
+      }
+    },
     createAssetPath(type, page, img) {
       return require(`@/assets/${type}/${page}/${img}`)
     },
