59278494fa2adefcecdd2da0095e71c8f86dc6ad
sveinugu <sveinugu@users.noreply.github.com>
2021-12-08 16:48:31 +0000

    Deploying to gh-pages from  @ 78050541ed42c6aee4ae8ed3e7fa59a6f46fabbd 🚀

---
+++ _nuxt/static/1638982106/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fcode","\u002Fcommunity","\u002Fmaterial","\u002Fnews","\u002Foverview","\u002Fpresenting","\u002Fservices","\u002Fstandards","\u002Ftopics"]})
