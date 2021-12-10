bcfa82598b5f9dd9da59b1488bef187c195ea1be
Sveinung Gundersen <sveinugu@gmail.com>
2021-12-10 16:54:03 +0100

    Added checks for frontmatter metadata

--- components/sections/AlternatingSubSections.vue
+++ components/sections/AlternatingSubSections.vue
@@ -9,6 +9,7 @@
     >
       <v-col cols="12" md="6" class="px-md-8 py-md-16 px-11 py-15 ma-auto">
         <v-img
+          v-if="subSection.img"
           :max-height="$vuetify.breakpoint.mdAndUp ? '900' : '500'"
           width="100%"
           contain
@@ -33,10 +34,12 @@
       >
         <v-container>
           <h2
+            v-if="subSection.title"
             class="text-h3 text-center font-weight-black"
             v-text="subSection.title"
           />
           <h3
+            v-if="subSection.ingress"
             class="text-h5 text-uppercase font-weight-light text-center my-8"
             v-text="subSection.ingress"
           />
@@ -49,7 +52,11 @@
             <v-icon size="24px" class="px-2">mdi-github</v-icon>
             <div class="font-weight-medium">GitHub repository:</div>
           </v-col>
-          <v-col cols="12" class="d-flex justify-center">
+          <v-col
+            v-if="subSection.github_link"
+            cols="12"
+            class="d-flex justify-center"
+          >
             <a :href="subSection.github_link"> {{ subSection.github_text }}</a>
           </v-col>
         </v-row>
