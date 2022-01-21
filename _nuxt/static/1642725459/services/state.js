360b84a9777a317569dc7fb00426f7df4bec19ad
sveinugu <sveinugu@users.noreply.github.com>
2022-01-21 00:57:24 +0000

    Deploying to gh-pages from  @ 8e97e223f65d39d3dc75bde7c28047bd4c6c77cf 🚀

--- _nuxt/static/1642725459/services/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ffairtracks.net\u002F_nuxt\u002Fstatic\u002F1642725459",layout:"default",error:a,state:{buttonHover:{buttonHoverId:""}},serverRendered:true,routePath:"\u002Fservices",config:{_app:{basePath:"\u002Ffairtracks.net\u002F",assetsPath:"\u002Ffairtracks.net\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"05f9c820"}}}}(null));
