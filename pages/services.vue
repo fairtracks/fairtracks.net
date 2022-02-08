c43bfc314bf8bd0d5ee180f39ef623cf7b46fe40
bianchini88 <fredebi@uio.no>
2022-02-08 16:04:17 +0100

    Placeholder for Blueprint/EPICO

--- pages/services.vue
+++ pages/services.vue
@@ -303,6 +303,45 @@ export default {
                   },
                 ],
               },
+		              {
+                logo: ['logos', 'blueprint-logo.jpeg'],
+                title: 'LOREM',
+                subtitle:
+                  'lorem',
+                icon: mdiMaterialDesign,
+                color: 'green darken-2',
+                features: [
+                  {
+                    icon: mdiDoorSlidingOpen,
+                    text: 'A common point of entry to register data collections into Ensembl and UCSC genome browsers',
+                  },
+                  {
+                    icon: mdiSwapHorizontal,
+                    text: 'The exchange format is a set of text files collectively referred to as a track hub',
+                  },
+                  {
+                    icon: mdiInformationVariant,
+                    text:
+                      ' Each track is assigned information such as its URL and reference genome. ' +
+                      'Additional attributes, including experimental metadata, can be attached',
+                  },
+                  {
+                    icon: mdiMarker,
+                    text: 'We propose highlighting FAIRtracks-compliant entries and making them easily available to downstream tools',
+                  },
+                  {
+                    icon: mdiApi,
+                    text: 'RESTful API for remote content querying',
+                  },
+                ],
+                services: [
+                  { text: 'Webpage', link: 'https://trackhubregistry.org/' },
+                  {
+                    text: 'REST API',
+                    link: 'https://www.trackhubregistry.org/docs/apis',
+                  },
+                ],
+              },
             ],
           },
         ],
