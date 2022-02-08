0bbf974365afba7b932ac02fb4ce60039f4559cd
bianchini88 <bianchini88@users.noreply.github.com>
2022-02-08 15:11:42 +0000

    Deploying to gh-pages from @ fairtracks/fairtracks.net@9f68fbf2eafb3c2bbbb0936ab34f26c0de246ec3 🚀

---
+++ _nuxt/static/1644333086/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fmodevue-demo\u002Fall-sections","\u002Fpresenting","\u002Ftopics","\u002Fmaterial","\u002Foverview","\u002Fcommunity","\u002Fstandards","\u002Fservices","\u002Fnews"]})
