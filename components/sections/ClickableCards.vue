d3b811abe9a9e7dd119903ff389a51eadff1a907
Sveinung Gundersen <sveinugu@gmail.com>
2022-02-10 14:28:48 +0100

    Reduced out-of-page effect. Mousedowns now pushed the cards down halfway

--- components/sections/ClickableCards.vue
+++ components/sections/ClickableCards.vue
@@ -7,13 +7,16 @@
           <v-card
             v-for="(card, index) in cards"
             :key="'c_' + index"
-            :elevation="hover ? 24 : 2"
+            :elevation="hover ? (down ? 6 : 12) : 2"
             :href="card.link ? card.link : null"
             :ripple="false"
-            :class="hover ? 'zoom' : 'notzoom'"
+            :class="hover ? (down ? 'halfzoom' : 'zoom') : 'notzoom'"
             class="transition-swing mx-auto"
             :max-width="card.maxWidth ? card.maxWidth : null"
             style="z-index: 2"
+            @mousedown="down = true"
+            @mouseup="down = false"
+            @mouseleave="down = false"
             @click="$emit('btn-click', true)"
           >
             <UiSmartImg
@@ -43,15 +46,23 @@ export default {
       default: () => [],
     },
   },
+  data() {
+    return { down: false }
+  },
 }
 </script>
 
-<style>
+<style scoped>
 .zoom {
-  transform: scale(1.025) translate(0, -10px);
-  transition: transform 0.2s;
+  transform: scale(1.0125) translate(0, -5px);
+  transition: transform 0.1s;
 }
+.halfzoom {
+}
+transform: scale(1.00625) translate(0, -2.5px);
+transition: transform 0.1s;
+
 .notzoom {
-  transition: transform 0.2s;
+  transition: transform 0.1s;
 }
 </style>
--- components/ui/InfoCard.vue
+++ components/ui/InfoCard.vue
@@ -1,14 +1,17 @@
 <template>
   <v-hover v-slot="{ hover }">
     <v-card
-      :elevation="hover ? 24 : 2"
+      :elevation="hover ? (down ? 6 : 12) : 2"
       outlined
       shaped
       width="380px"
       height="586px"
       :href="card.services[card.services.length - 1].link"
-      :class="hover ? 'zoom' : 'notzoom'"
+      :class="hover ? (down ? 'halfzoom' : 'zoom') : 'notzoom'"
       class="py-4 px-4 mx-auto transition-swing"
+      @mousedown="down = true"
+      @mouseup="down = false"
+      @mouseleave="down = false"
       ><div>
         <v-responsive height="170" class="pt-3 px-1">
           <v-row no-gutters class="fill-height pa-0">
@@ -103,16 +106,23 @@ export default {
       default: () => {},
     },
   },
+  data() {
+    return { down: false }
+  },
 }
 </script>
 
 <style scoped>
 .zoom {
-  transform: scale(1.025) translate(0, -10px);
-  transition: transform 0.2s;
+  transform: scale(1.0125) translate(0, -5px);
+  transition: transform 0.1s;
+}
+.halfzoom {
+  transform: scale(1.00625) translate(0, -2.5px);
+  transition: transform 0.1s;
 }
 .notzoom {
-  transition: transform 0.2s;
+  transition: transform 0.1s;
 }
 .text-h6 {
   line-height: 1.7rem !important;
