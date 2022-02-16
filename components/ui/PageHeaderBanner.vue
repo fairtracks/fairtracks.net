0baca76b00c2e208e04d0755eff18e52242e1f0a
Sveinung Gundersen <sveinugu@gmail.com>
2022-02-16 21:32:39 +0100

    Fixed scaling issues with PageHeaderBanner

--- components/ui/PageHeaderBanner.vue
+++ components/ui/PageHeaderBanner.vue
@@ -1,31 +1,39 @@
 <template>
-  <v-responsive :aspect-ratio="567 / 56.3" max-height="150px" min-height="75px">
-    <v-row class="ma-0" no-gutters>
-      <v-col v-for="(imgSrc, i) in pageHeaderImages" :key="i" cols="4">
-        <v-row justify="center" class="ma-0 gradient-fill-header">
-          <v-responsive
-            width="33%"
-            height="100%"
-            :aspect-ratio="189 / 56.3"
-            max-height="150px"
-            min-height="75px"
-            style="z-index: 2"
-          >
-            <UiSmartBackgroundImg
-              height="56.3"
-              width="189"
-              min-height="75"
-              :image-asset="imgSrc"
-            />
-          </v-responsive>
-        </v-row>
-      </v-col>
-    </v-row>
-    <v-overlay absolute opacity="0" z-index="3">
-      <h1 class="text-md-h2 text-sm-h3 text-h4 font-weight-black text-center">
-        {{ pageHeader.toUpperCase() }}
-      </h1>
-    </v-overlay>
+  <v-responsive
+    max-height="125px"
+    min-height="75px"
+    class="gradient-fill-header"
+  >
+    <v-responsive :aspect-ratio="567 / 56.3" max-width="1258.9" class="mx-auto">
+      <v-row class="ma-0" no-gutters>
+        <v-col v-for="(imgSrc, i) in pageHeaderImages" :key="i" cols="4">
+          <v-row no-gutters justify="center">
+            <v-responsive
+              width="33%"
+              height="100%"
+              :aspect-ratio="189 / 56.3"
+              max-height="150px"
+              min-height="75px"
+              style="z-index: 2"
+              class="gradient-fill-header"
+            >
+              <UiSmartBackgroundImg
+                height="56.3"
+                width="189"
+                min-height="75"
+                :image-asset="imgSrc"
+                contain
+              />
+            </v-responsive>
+          </v-row>
+        </v-col>
+      </v-row>
+      <v-overlay absolute opacity="0" z-index="3">
+        <h1 class="text-md-h2 text-sm-h3 text-h4 font-weight-black text-center">
+          {{ pageHeader.toUpperCase() }}
+        </h1>
+      </v-overlay>
+    </v-responsive>
   </v-responsive>
 </template>
 
@@ -45,7 +53,7 @@ export default {
 </script>
 
 <style>
-.gradient-fill-header .v-responsive__content {
+.gradient-fill-header > .v-responsive__content {
   background: linear-gradient(
     to bottom,
     rgba(var(--v-accent-rgb), 0.9),
