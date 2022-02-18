83aa4bd5951a8d8a769ff222a0fb8e09831f7d6e
Sveinung Gundersen <sveinugu@gmail.com>
2022-02-18 17:17:58 +0100

    Fixed loading of variables.scss. Not needed after all, but may be usable in the future

--- nuxt.config.js
+++ nuxt.config.js
@@ -128,7 +128,7 @@ export default {
 
   // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
   vuetify: {
-    // customVariables: ['~/assets/variables.scss'],
+    customVariables: ['~/assets/style/variables.scss'],
     defaultAssets: {
       font: {
         family: 'Roboto',
--- plugins/load-nonimage-assets.js
+++ plugins/load-nonimage-assets.js
@@ -1,5 +1,11 @@
+function _getNonImageFilesFunc() {
+  // Other file formats can be added with OR, e.g. "/\.(pdf|mpeg)$/"
+  return require.context(`@/assets`, true, /\.(pdf)$/)
+}
+
 function _getNonImageAssetPath(category, page, filename) {
-  return require(`@/assets/${category}/${page}/${filename}`)
+  const requirePath = `./${category}/${page}/${filename}`
+  return _getNonImageFilesFunc()(requirePath)
 }
 
 export default ({ _app }, inject) => {
