b0e4fabe0a25d344b6c161dc17ee36ce92da5f3d
Nazeeefa <fatima.nazeefa@gmail.com>
2022-02-24 16:16:49 +0100

    Partly redesigned material cards. Added descriptions and button icons. Added partly updated material content

--- components/ui/MaterialCard.vue
+++ components/ui/MaterialCard.vue
@@ -1,27 +1,42 @@
 <template>
-  <v-card width="380" class="mx-auto">
-    <v-responsive height="280">
+  <v-card width="550" height="auto" class="mx-auto-center">
       <v-row justify="center" no-gutters class="ma-2">
-        <v-col cols="6">
+    <v-responsive height="400">
+
           <UiZoomableImage
             :image-asset="
               $getImageAssetObject('materials', 'previews', card.previewImg)
             "
-            max-height="300"
+            max-height="200px"
             :dialog-buttons="card.dialogButtons"
             crop-bottom
           />
-        </v-col>
-        <v-col cols="6">
           <v-card-title
-            class="text-subtitle-1 text-left font-weight-bold card-title"
+            class="text-subtitle-1 text-center font-weight-bold card-title"
           >
-            {{ card.title }}
+      {{ card.title }}
           </v-card-title>
-        </v-col>
-      </v-row>
+          <v-card-text>
+      {{ card.description }}
+          </v-card-text>
+        </v-responsive>
+        <v-responsive height="100" width="100%">
+          <v-list class="transparent pa-0" max-width="100%">
+          <v-list-item
+            v-for="(infoItem, index) in card.infoItems"
+            :key="'c_' + index"
+          >
+            <v-list-item-icon>
+              <v-icon :color="$vuetify.theme.dark ? 'accent' : 'primary'" v-text="infoItem.icon"/>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title class="text-wrap" v-text="infoItem.text"/>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
     </v-responsive>
-    <v-card-actions>
+      </v-row>
+      <v-card-actions>
       <v-col cols="12" align-self="end">
         <v-row
           no-gutters
@@ -43,6 +58,8 @@
               class="text-weight-light"
               :href="button.link"
               :text="button.text"
+              :icon="button.icon"
+              small
             />
           </v-col>
         </v-row> </v-col
