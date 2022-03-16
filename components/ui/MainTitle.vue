1bdcbf77f7bbb712a70b3b02fe60bb7b21a0974f
Sveinung Gundersen <sveinugu@gmail.com>
2022-03-16 13:32:44 +0100

    Added option to scale down MainTitle elements at viewport size 'sm' instead of default 'md'

--- components/ui/MainTitle.vue
+++ components/ui/MainTitle.vue
@@ -2,17 +2,20 @@
   <div>
     <h2
       v-show="title"
-      class="text-h4 text-md-h3 text-center font-weight-black mt-4 mb-8"
+      class="text-h4 text-center font-weight-black mt-4 mb-8"
+      :class="scaleDownAtSm ? 'text-sm-h3' : 'text-md-h3'"
       v-text="title"
     />
     <h3
       v-show="subtitle"
-      class="text-h6 text-md-h5 font-weight-medium font-italic text-center mb-8"
+      class="text-h6 font-weight-medium font-italic text-center mb-8"
+      :class="scaleDownAtSm ? 'text-sm-h5' : 'text-md-h5'"
       v-text="subtitle"
     />
     <h4
       v-show="ingress"
-      class="text-h6 font-weight-medium font-italic text-center mb-4"
+      class="text-h6 text-center mb-4"
+      :class="subtitle ? 'font-weight-light' : 'font-weight-medium font-italic'"
       v-html="compileMarkdown(ingress)"
     />
     <p
@@ -43,6 +46,10 @@ export default {
     info: {
       type: String,
       default: '',
+    },
+    scaleDownAtSm: {
+      type: Boolean,
+      default: false,
     },
   },
   methods: {
