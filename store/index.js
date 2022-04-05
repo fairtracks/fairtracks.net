eaab7d9a85770f5dc38411c23a0db5de58fcc4b5
Sveinung Gundersen <sveinugu@gmail.com>
2022-04-05 15:33:55 +0200

    Bugfix: Github API calls no longer happens on client

--- store/index.js
+++ store/index.js
@@ -2,7 +2,8 @@ import githubActions from '~/store/github/actions'
 
 export const actions = {
   async nuxtServerInit(store, context) {
-    console.assert(process.server)
-    await githubActions.nuxtServerInit(store, context)
+    if (process.server) {
+      await githubActions.nuxtServerInit(store, context)
+    }
   },
 }
