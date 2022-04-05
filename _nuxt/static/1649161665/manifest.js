7bead8a107a006f168350ebd14e03aed9e40ecf7
sveinugu <sveinugu@users.noreply.github.com>
2022-04-05 12:28:15 +0000

    Deploying to gh-pages from @ fairtracks/fairtracks.net@f69c1d3fc2d752236b51163d6e0a244b72b2ec80 🚀

---
+++ _nuxt/static/1649161665/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Ftopics","\u002Fstandards","\u002Fcommunity","\u002Fnews","\u002Fmaterial","\u002Fpresenting","\u002Fservices","\u002Foverview","\u002Fcode","\u002F","\u002Fmodevue-demo\u002Fall-sections"]})
