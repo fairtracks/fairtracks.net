d32be669f0f881ee394f84843120e7fb0898da6a
Sveinung Gundersen <sveinugu@gmail.com>
2022-04-06 16:02:30 +0200

    Renamed Overview.vue to OverviewLayout.vue (with the old component renamed to OldOverviewLayout.vue). Misc renaming and other cleanup. Fixed line lengths.

--- components/sections/OverviewLayout.vue
+++ components/sections/OverviewLayout.vue
@@ -1,72 +1,75 @@
 <template>
-  <section :id="sectionId">
-    <UiMainTitle :title="sectionTitle" />
-    <v-row class="fill-height" justify="space-around">
-      <v-col cols="4">
-        <p v-html="compileMarkdown(introText)" />
-      </v-col>
-
-      <v-col v-for="col_index in [1, 2]" :key="col_index" cols="4">
-        <v-row>
-          <v-col
-            v-for="(referenceList, r_index) in slicedReferenceLists[col_index - 1]"
-            :key="r_index"
-            cols="12"
+  <section class="py-16" :style="cssVars">
+    <v-container grid-list-sm fluid>
+      <v-row>
+        <v-col class="text-center">
+          <h2 class="text-h4 text-md-h3 text-center font-weight-black">
+            {{ header }}
+          </h2>
+          <p class="text-center text-p px-10 py-4">
+            {{ introText }}
+          </p>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col v-for="(card, index) in cards" :key="index" cols="12" sm="3" class="text-center">
+          <v-avatar
+            size="100"
+            color="primary lighten-1"
+            class="font-weight-bold mb-5"
+            style="opacity: 1.15"
           >
-            <UiListOfReferences
-              :list-header="referenceList.header"
-              :reference-list="referenceList.references"
-              :reference-store="referenceStore"
-            />
-          </v-col>
-        </v-row>
-      </v-col>
-    </v-row>
+            <span class="v-avatar-text">{{ card.title }}</span>
+          </v-avatar>
+          <v-row v-for="(cardItem, itemIndex) in card.items" :key="itemIndex" class="text-center">
+            <v-col class="text-center">
+              <h3>{{ cardItem.title }}</h3>
+              <p>{{ cardItem.description }}</p>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
   </section>
 </template>
 
 <script>
-import { marked } from 'marked'
-
 export default {
   props: {
-    sectionId: {
+    header: {
       type: String,
-      default: '',
-    },
-    sectionTitle: {
-      type: String,
-      default: '',
+      required: true,
     },
     introText: {
       type: String,
       default: '',
     },
-    referenceLists: {
+    cards: {
       type: Array,
       default: () => [],
     },
-    referenceStore: {
-      type: Object,
-      default: () => {},
-    },
-    listsInSecondCol: {
-      type: Number,
-      default: 2,
+    backgroundColorState: {
+      type: Boolean,
+      default: false,
     },
   },
   computed: {
-    slicedReferenceLists() {
-      return [
-        this.referenceLists.slice(0, this.listsInSecondCol),
-        this.referenceLists.slice(this.listsInSecondCol),
-      ]
-    },
-  },
-  methods: {
-    compileMarkdown(string) {
-      return marked.parseInline(string, [])
+    cssVars() {
+      if (this.backgroundColorState === true) {
+        return {
+          'background-color': 'aliceBlue',
+          color: 'black',
+        }
+      } else {
+        return { 'background-color': '' }
+      }
     },
   },
 }
 </script>
+
+<style scoped>
+.v-avatar-text {
+  color: white;
+}
+</style>
