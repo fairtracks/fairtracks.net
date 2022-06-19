ce6cf6a690e1bc0b8d58a674e04635dc385f45a9
Sveinung Gundersen <sveinugu@gmail.com>
2022-06-19 17:09:28 +0200

    Fixed scrolling. Removed smooth scroll. Anchor scrolling sometimes fails in dev but now mostly works in production

--- app/router.scrollBehavior.js
+++ app/router.scrollBehavior.js
@@ -1,39 +1,40 @@
-import goTo from 'vuetify/lib/services/goto'
-import { getMatchedComponents, setScrollRestoration } from './utils'
-
+// import { getMatchedComponents } from './utils'
+//
 if (process.client) {
   if ('scrollRestoration' in window.history) {
-    setScrollRestoration('manual')
+    window.history.scrollRestoration = 'manual'
 
     // reset scrollRestoration to auto when leaving page, allowing page reload
     // and back-navigation from other pages to use the browser to restore the
     // scrolling position.
     window.addEventListener('beforeunload', () => {
-      setScrollRestoration('auto')
+      window.history.scrollRestoration = 'auto'
     })
 
     // Setting scrollRestoration to manual again when returning to this page.
     window.addEventListener('load', () => {
-      setScrollRestoration('manual')
+      window.history.scrollRestoration = 'manual'
     })
   }
 }
+//
+// function shouldScrollToTop(route) {
+//   const Pages = getMatchedComponents(route)
+//   if (Pages.length === 1) {
+//     const { options = {} } = Pages[0]
+//     return options.scrollToTop !== false
+//   }
+//   return Pages.some(({ options }) => options && options.scrollToTop)
+// }
 
-function shouldScrollToTop(route) {
-  const Pages = getMatchedComponents(route)
-  if (Pages.length === 1) {
-    const { options = {} } = Pages[0]
-    return options.scrollToTop !== false
-  }
-  return Pages.some(({ options }) => options && options.scrollToTop)
-}
-
-export default function (to, from, savedPosition) {
+export default function scrollBehavior(to, from, savedPosition) {
   // If the returned position is falsy or an empty object, will retain current scroll position
   let position = false
   const isRouteChanged = to !== from
-  const differentPage = to.path !== from.path
-  const samePageDifferentHash = to.path === from.path && to.hash !== from.hash
+  // const differentPage = to.path !== from?.path
+  const samePageDifferentHash = to.path === from?.path && to.hash !== from?.hash
+
+  // console.log(isRouteChanged, differentPage, samePageDifferentHash)
 
   const nuxt = window.$nuxt
 
@@ -46,48 +47,66 @@ export default function (to, from, savedPosition) {
     nuxt.$nextTick(() => nuxt.$emit('triggerScroll'))
   }
 
-  return new Promise((resolve) => {
-    // wait for the out transition to complete (if necessary)
-    nuxt.$once('triggerScroll', () => {
-      // coords will be used if no selector is provided,
-      // or if the selector didn't match any element.
-      if (differentPage && shouldScrollToTop(to)) {
-        window.scrollTo(0, 0)
+  // wait for the out transition to complete (if necessary)
+  nuxt.$once('triggerScroll', async () => {
+    // coords will be used if no selector is provided,
+    // or if the selector didn't match any element.
+
+    // if (differentPage) {
+    //   window.scrollTo(0, 0)
+    // }
+
+    if (savedPosition) {
+      // savedPosition is only available for popstate navigations (back button)
+      position = { x: savedPosition.x, y: savedPosition.y }
+    } else if (to.hash) {
+      let hash = to.hash
+      // CSS.escape() is not supported with IE and Edge.
+      if (typeof window.CSS !== 'undefined' && typeof window.CSS.escape !== 'undefined') {
+        hash = '#' + window.CSS.escape(hash.substring(1))
       }
 
-      if (savedPosition) {
-        // savedPosition is only available for popstate navigations (back button)
-        position = { x: savedPosition.x, y: savedPosition.y + 64 }
-      } else if (to.hash) {
-        let hash = to.hash
-        // CSS.escape() is not supported with IE and Edge.
-        if (typeof window.CSS !== 'undefined' && typeof window.CSS.escape !== 'undefined') {
-          hash = '#' + window.CSS.escape(hash.substring(1))
-        }
-        try {
-          const element = document.querySelector(hash)
-          if (element) {
-            // scroll to anchor by returning the selector
-            position = {
-              selector: hash,
-            }
-          }
-        } catch (e) {
-          console.warn(
-            'Failed to save scroll position. Please add CSS.escape() polyfill (https://github.com/mathiasbynens/CSS.escape).'
+      try {
+        const findEl = (hash, x) => {
+          return (
+            document.querySelector(hash) ||
+            new Promise((resolve) => {
+              if (x > 10) {
+                return resolve()
+              }
+              setTimeout(() => {
+                resolve(findEl(hash, ++x || 1))
+              }, 100)
+            })
           )
         }
-      } else {
-        position = { x: 0, y: 0 }
-      }
 
-      if (typeof position.y !== 'undefined') {
-        goTo(position.y)
-      } else if (typeof position.selector !== 'undefined') {
-        goTo(position.selector)
+        const el = await findEl(to.hash, 0)
+        if (el) {
+          // scroll to anchor by returning the selector
+          position = { selector: hash }
+          // Respect any scroll-margin-top set in CSS when scrolling to anchor
+          const y = Number(getComputedStyle(el)['scroll-margin-top']?.replace('px', ''))
+          if (y) {
+            position.offset = { y }
+          }
+        }
+      } catch (e) {
+        console.warn(
+          'Failed to save scroll position. Please add CSS.escape() polyfill (https://github.com/mathiasbynens/CSS.escape).'
+        )
       }
+      // } else if (isRouteChanged && shouldScrollToTop(to)) {
+    } else if (isRouteChanged) {
+      position = { x: 0, y: 0 }
+    }
 
-      resolve(position)
-    })
+    if (typeof position.y !== 'undefined') {
+      window.scrollTo(position.x, position.y)
+    } else if (typeof position.selector !== 'undefined') {
+      document.getElementById(position.selector.substring(1)).scrollIntoView(true)
+    }
+
+    return position
   })
 }
