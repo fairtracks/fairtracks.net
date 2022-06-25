43166ac0139c0dc680250861f80a2773bf3e7475
Sveinung Gundersen <sveinugu@gmail.com>
2022-06-25 12:46:26 +0200

    Cleanup of vuex store code

--- store/buttonHover.js
+++ store/buttonHover.js
@@ -1,9 +1,15 @@
+const NAMESPACE = 'buttonHover/'
+
+export const M_SET_ID = 'setId'
+
+export const BUTTON_HOVER_M_SET_ID = NAMESPACE + M_SET_ID
+
 export const state = () => ({
   buttonHoverId: '',
 })
 
 export const mutations = {
-  setButtonHoverId(state, id) {
+  [M_SET_ID]: (state, id) => {
     state.buttonHoverId = id
   },
 }
