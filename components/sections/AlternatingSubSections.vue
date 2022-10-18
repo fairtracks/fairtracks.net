3170e7dc8a081757080e5718ae57ec34c2f6c112
Sveinung Gundersen <sveinugu@gmail.com>
2022-10-18 10:34:31 +0200

    Fixed figure placement without javascript (server-side)

--- components/sections/AlternatingSubSections.vue
+++ components/sections/AlternatingSubSections.vue
@@ -3,11 +3,7 @@
     <section v-for="(subSection, s_index) in markdownFiles" :id="subSection.slug" :key="s_index">
       <v-row class="pa-0 pt-md-16 pt-8" />
       <v-row no-gutters :class="{ 'flex-row-reverse': s_index % 2 === 1 }">
-        <UiFigureList
-          v-if="$vuetify.breakpoint.lgAndUp"
-          :sub-section="subSection"
-          :sub-section-index="s_index"
-        />
+        <UiFigureList class="lg-and-up" :sub-section="subSection" :sub-section-index="s_index" />
         <v-col cols="12" lg="6" align-self="center" class="px-md-8 px-4 pb-0">
           <v-row no-gutters justify="center" class="pa-3">
             <v-col cols="12">
@@ -20,11 +16,7 @@
           </v-row>
         </v-col>
       </v-row>
-      <UiFigureList
-        v-if="$vuetify.breakpoint.mdAndDown"
-        :sub-section="subSection"
-        :sub-section-index="s_index"
-      />
+      <UiFigureList class="md-and-down" :sub-section="subSection" :sub-section-index="s_index" />
       <UiTableList :sub-section="subSection" :sub-section-index="s_index" />
     </section>
   </div>
@@ -49,3 +41,29 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+@media (min-width: 1264px) {
+  .lg-and-up {
+    display: block;
+  }
+}
+
+@media (max-width: 1263px) {
+  .lg-and-up {
+    display: none;
+  }
+}
+
+@media (min-width: 1264px) {
+  .md-and-down {
+    display: none;
+  }
+}
+
+@media (max-width: 1263px) {
+  .md-and-down {
+    display: block;
+  }
+}
+</style>
