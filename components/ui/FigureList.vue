21482c602dc46c7c372ad8eecfe9e004d59a573b
Sveinung Gundersen <sveinugu@gmail.com>
2022-11-03 03:58:16 +0100

    Added logic for figure adjustment depending on viewport size

--- components/ui/FigureList.vue
+++ components/ui/FigureList.vue
@@ -3,7 +3,7 @@
     cols="12"
     lg="6"
     class="px-md-15 px-11 py-0"
-    :align-self="subSection.tables ? 'end' : 'center'"
+    :class="subSection.tables ? 'vl-and-up-align-self-end-else-center' : 'align-self-center'"
   >
     <figure
       v-for="(figure, figIndex) in subSection.figures"
@@ -68,3 +68,17 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+/* Custom styling for custom breakpoint vl ("very large") == 1640px*/
+@media (min-width: 1640px) {
+  .vl-and-up-align-self-end-else-center {
+    align-self: end;
+  }
+}
+@media (max-width: 1639px) {
+  .vl-and-up-align-self-end-else-center {
+    align-self: center;
+  }
+}
+</style>
