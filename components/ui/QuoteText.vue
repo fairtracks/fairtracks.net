3cb880bc0e786d56464cf653a4bbbd9f93f0ba1a
Sveinung Gundersen <sveinugu@gmail.com>
2022-11-06 16:41:19 +0100

    Added support for Quote citations

--- components/ui/QuoteText.vue
+++ components/ui/QuoteText.vue
@@ -1,25 +1,36 @@
 <template>
-  <v-row class="pa-4">
-    <v-col cols="1">
-      <UiSmartIcon name="format-quote-open" x-large :class="themeBasedClass(true)" />
-    </v-col>
-    <v-col cols="10">
-      <p
-        :class="themeBasedClass(false)"
-        class="ma-0 text-h6 font-italic text-center"
-        v-text="quote"
-      />
-    </v-col>
-    <v-col cols="1">
-      <UiSmartIcon name="format-quote-close" x-large :class="themeBasedClass(true)" />
-    </v-col>
-  </v-row>
+  <div class="pa-4">
+    <v-row>
+      <v-col cols="1">
+        <UiSmartIcon name="format-quote-open" x-large :class="themeBasedClass(true)" />
+      </v-col>
+      <v-col cols="10">
+        <p
+          :class="themeBasedClass(false)"
+          class="ma-0 text-h6 font-italic text-center"
+          v-text="quote"
+        />
+      </v-col>
+      <v-col cols="1">
+        <UiSmartIcon name="format-quote-close" x-large :class="themeBasedClass(true)" />
+      </v-col>
+    </v-row>
+    <v-row v-if="citation" class="pt-2 px-4 justify-end">
+      <p class="body-2 font-italic" v-html="compileMarkdown(citation)"></p>
+    </v-row>
+  </div>
 </template>
 
 <script>
+import MarkdownSupport from '~/mixins/markdown-support'
 export default {
+  mixins: [MarkdownSupport],
   props: {
     quote: {
+      type: String,
+      default: '',
+    },
+    citation: {
       type: String,
       default: '',
     },
