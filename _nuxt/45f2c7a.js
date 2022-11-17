517b64c4e01e6fa60013ace865955dc2ffa936fa
sveinugu <sveinugu@users.noreply.github.com>
2022-11-17 15:31:39 +0000

    Deploying to gh-pages from @ fairtracks/fairtracks.github.io@a875abf31737ecef45b0f50c16917934cf5c24da 🚀

---
+++ _nuxt/45f2c7a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[63],{1519:function(n,t,e){"use strict";e.r(t);var u={asyncData:function(n){return(0,n.redirect)("/community/")},data:function(){return{componentId:"contact"}}},c=e(61),o=Object(c.a)(u,undefined,undefined,!1,null,null,null);t.default=o.exports}}]);
