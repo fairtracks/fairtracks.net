201e05ca4338076604353b36cd7b8e35edf8c930
sveinugu <sveinugu@users.noreply.github.com>
2023-03-23 23:26:33 +0000

    Deploying to gh-pages from @ fairtracks/fairtracks.github.io@bdf594d3dd1dcb55ce7453f4fb999d5b8c5d7fca 🚀

--- _nuxt/static/1679490016/survey/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1679490016",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fsurvey\u002F",config:{optimizeImages:true,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9193e2f8"}}}}(null));
